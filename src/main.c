0b1757c8148335f98b76bf8769f6078b381b2c84
Paul Bie <pbie@student.42.fr>
2017-09-14 19:50:20 +0200

    [Add] - interrogate

--- src/main.c
+++ src/main.c
@@ -12,6 +12,43 @@
 
 #include "ft_select.h"
 
+void						interrogate(void)
+{
+	char					*cl_string;
+	char					*cm_string;
+	int					height;
+	int					width;
+	int					auto_wrap;
+	char					pc;
+	char					*bc;
+	char					*up;
+	char					*temp;
+
+	#ifdef Unix
+	/* Here we assume that an explicit term_buffer
+		was provided to tgetent.  */
+	char *buffer
+		= (char *) malloc (strlen (term_buffer));
+	#define BUFFADDR &buffer
+	#else
+	#define BUFFADDR 0
+	#endif
+
+	//Extract information we will use
+	cl_string = tgetstr("cl", BUFFADDR);
+	cm_string = tgetstr("cm", BUFFADDR);
+	auto_wrap = tgetflag("am");
+	height = tgetnum("li");
+	width = tgetnum("co");
+
+	//Extract information that termcap functions use
+	temp = tgetstr("pc", BUFFADDR);
+	pc = temp? *temp : 0;
+	bc = tgetstr("le", BUFFADDR);
+	up = tgetstr("up", BUFFADDR);
+	ft_putendl("end of interrogate");
+}
+
 int						which_key(void)
 {
 	char					buffer[3];
@@ -57,6 +94,7 @@ int						main(int ac, char **av, char **ev)
 	// remplis la structure termios des possibilités du terminal.
 	if (tcgetattr(0, &term) == -1)
 		return (-1);
+	interrogate();
 	which_key();
 	return (0);
 }
